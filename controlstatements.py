48f6da4c2d4bfd583663f98cabcb7a1aa3fdfcc2
venkatisukapalli <venkatisukapalli@gmail.com>
2024-08-14 13:26:24 +0530

    control

---
+++ controlstatements.py
@@ -0,0 +1,17 @@
+user_name=input("Enter user_name: ")
+password=input("Enter password: ")
+if user_name == "admin" and password == "admin" :
+    print("Welcome Admin!")
+else :
+    print("failed")
+
+
+
+a= 4
+b= 5
+if a>b :
+    print("a is less than b")
+else :
+    print("b is greater than a")
+
+
